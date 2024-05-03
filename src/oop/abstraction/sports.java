1dbb0d997d1a88bdf1b3fba48b445eb950204ac5
BrianCeasar <brianceasar7@gmail.com>
2024-05-03 14:06:34 +0300

    Add Sports interface The Sports interface defines abstract methods representing various aspects of a sports game Changes: - Added Sports interface - Defined abstract methods for representing sports-related behaviors

---
+++ src/oop/abstraction/sports.java
@@ -0,0 +1,13 @@
+package oop.abstraction;
+
+public interface Sports{
+    public abstract void materials ();
+	
+	public abstract void teams ();
+	
+	public abstract void players ();
+	
+	public abstract void time();
+	
+	public abstract void tieRules();
+}
