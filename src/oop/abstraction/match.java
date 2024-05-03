529fa7d5c7a3c5ae539eb992dc45a3e0a8f13b8f
BrianCeasar <brianceasar7@gmail.com>
2024-05-03 12:15:19 +0300

    Add Match class The Match class contains a main method that demonstrates the usage of abstraction in OOP by creating instances of the Sports and Referee interfaces It initializes a Soccer object as a Sports instance, then calls various methods defined in the Sports interface to demonstrate polymorphism and abstraction. Changes: Add match Class Implemented main method to demonstrate abstraction and polymorphism Created instances of Soccer class to demonstrate abstraction

---
+++ src/oop/abstraction/match.java
@@ -0,0 +1,16 @@
+package oop.abstraction;
+
+public class Match{
+    public static void main(string[] args){
+        Sports sports = new Soccer();
+
+        sports.materials();
+		sports.teams();
+		sports.players();
+		sports.time();
+		sports.tieRules();
+
+        Referee referee = new Soccer();
+        referee.referee();
+    }
+}
